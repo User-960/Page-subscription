a4e821b73badc5b76e5ef5a5d65c3ce653ce1224
Ivan <ivanvolodin960@gmail.com>
2023-12-29 02:14:21 +0300

    feat: add main section

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
+import Form from '@/components/ui/form/Form'
+
 import Layout from '@/components/layout/Layout'
 import { IMeta } from '@/components/seo/meta.interface'
+
+import styles from './Home.module.scss'
 
 const Home = () => {
 	const meta: IMeta = {
@@ -9,7 +13,25 @@ const Home = () => {
 		description: 'main page'
 	}
 
-	return <Layout meta={meta}>Page Home</Layout>
+	return (
+		<Layout meta={meta}>
+			<section className='flex'>
+				<div className='px-56'>
+					<div className={styles.cycleShadow}></div>
+					<h1 className={styles.title}>
+						Unlock valuable insights from subscription data
+					</h1>
+					<p className={styles.subTitle}>
+						Analyze your subscription ecosystem for cost optimization and risk
+						management
+					</p>
+					<Form />
+				</div>
+
+				<div>Section</div>
+			</section>
+		</Layout>
+	)
 }
 
 export default Home
