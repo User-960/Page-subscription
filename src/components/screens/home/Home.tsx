2dae2f3cdf759d0dc5ea7c20e0a23889c5af2d41
Ivan <ivanvolodin960@gmail.com>
2023-12-31 00:56:52 +0300

    feat: add one more section in Home component

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -7,6 +7,8 @@ import { IMeta } from '@/components/seo/meta.interface'
 
 import styles from './Home.module.scss'
 
+const cn = require('clsx')
+
 const Home = () => {
 	const meta: IMeta = {
 		title: 'Main Page',
@@ -40,7 +42,25 @@ const Home = () => {
 					/>
 				</div>
 
-				<div>Section</div>
+				<div>
+					<div className={styles.document}>
+						<h6 className={styles.titleDoc}>Acme Corp.</h6>
+						<div className={cn(styles.line, 'mt-3.5', 'mb-4')}></div>
+						<div className='flex'>
+							<div>
+								<p className={styles.address}>Acme Software Co.</p>
+								<p className={styles.address}>50 E Rio Grande Pkwy</p>
+								<p className={styles.address}>Tempe, AZ 85281 United States</p>
+							</div>
+							<div className='ml-9'>
+								<p className={styles.address}>Wasatch Cloud Inc.</p>
+								<p className={styles.address}>18560 West Sunset Blvd.</p>
+								<p className={styles.address}>LOS Angeles, CA 90046 US</p>
+							</div>
+						</div>
+						<div className={cn(styles.line, 'mt-4', 'mb-6')}></div>
+					</div>
+				</div>
 			</section>
 		</Layout>
 	)
