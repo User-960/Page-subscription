ad81bbc19b4ce44ea933753efb7c0a8f1667e848
Ivan <ivanvolodin960@gmail.com>
2024-01-16 05:31:14 +0300

    feat: add adaptive for some pages

--- src/config/app.constants.ts
+++ src/config/app.constants.ts
@@ -1,6 +1,8 @@
 // export const SERVER_URL = process.env.SERVER_URL
+// export const COINGECKO_URL = process.env.COIN_GECKO_URL
+// export const CRYPTO_COMPARE_URL = process.env.CRYPTO_COMPARE_URL
+
 export const SERVER_URL = 'http://localhost:5000'
-// export const COINGECKO_URL = process.env.COINGECKO_URL
 export const COINGECKO_URL = 'https://api.coingecko.com'
 export const CRYPTO_COMPARE_URL = 'https://min-api.cryptocompare.com/data/v2'
 
