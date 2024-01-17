c3e139d8fc291aab63609a73dd25ad0644bd5a6a
Ivan <ivanvolodin960@gmail.com>
2024-01-17 03:43:50 +0300

    fix: delete unused vars

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 
@@ -7,26 +6,16 @@ import '@/assets/styles/index.scss'
 import store from '@/store'
 import { ColorModeContext, useMode } from '@/theme/theme'
 
-const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			refetchOnWindowFocus: false
-		}
-	}
-})
-
 export default function App({ Component, pageProps }: AppProps) {
 	const [theme, colorMode] = useMode()
 	return (
 		<Provider store={store}>
-			<QueryClientProvider client={queryClient}>
-				<ColorModeContext.Provider value={colorMode}>
-					<ThemeProvider theme={theme}>
-						<CssBaseline />
-						<Component {...pageProps} />
-					</ThemeProvider>
-				</ColorModeContext.Provider>
-			</QueryClientProvider>
+			<ColorModeContext.Provider value={colorMode}>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Component {...pageProps} />
+				</ThemeProvider>
+			</ColorModeContext.Provider>
 		</Provider>
 	)
 }
